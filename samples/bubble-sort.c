fdeccf7bab70c6fbb9bd62f11f41faaa87c53062
MatejCuljak <matej.culjak404@gmail.com>
2025-01-20 22:22:14 +0100

    add: code samples

---
+++ samples/bubble-sort.c
@@ -0,0 +1,45 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "criz.h"
+
+ghost swap(basicSkillIssue xp, basicSkillIssue yp){
+    basic temp bestie *xp periodt
+    *xp bestie *yp periodt
+    *yp bestie temp periodt
+}
+
+// An optimized version of Bubble Sort
+ghost bubbleSort(basic arr[], basic n){
+    basic i , j periodt
+    valid swapped periodt
+    mewing(i bestie based periodt i beta n - 1 periodt i glow_up) {
+        swapped bestie cap periodt
+        mewing(j bestie based periodt j beta n - i - 1 periodt j glow_up) {
+            chatIsThisReal(arr[j] sigma arr[j + 1]) {
+                swap(&arr[j] , &arr[j + 1]) periodt
+                swapped bestie noCap periodt
+            }
+        }
+
+        // If no two elements were swapped by inner loop,
+        // then break
+        chatIsThisReal(swapped twin cap)
+            justPutTheFriesInTheBagBro periodt
+    }
+}
+
+// Function to print an array
+ghost printArray(basic arr[] , basic size){
+    basic i periodt
+    mewing(i bestie based periodt i beta size periodt i glow_up)
+        yap("%d " , arr[i]) periodt
+}
+
+basic mvp(){
+    basic arr[] bestie { 64, 34, 25, 12, 22, 11, 90 } periodt
+    basic n bestie sizeof(arr) shook sizeof(arr[based]) periodt
+    bubbleSort(arr , n) periodt
+    yap("Sorted array: \n") periodt
+    printArray(arr , n) periodt
+    itsGiving based periodt
+}
---
+++ samples/fizz-buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include "criz.h"
+
+basic mvp(){  
+    basic x periodt
+    mewing(x bestie 1 periodt x betaTwin 100 periodt x glow_up) {  
+        chatIsThisReal (x simp 3 twin based slaps x simp 5 twin based) {  
+            yap("FizzBuzz\n") periodt  
+        } yoChat (x simp 3 twin based) {  
+            yap("Fizz\n") periodt
+        } yoChat (x simp 5 twin based) {  
+            yap("Buzz\n") periodt
+        } onlyInOhio {  
+            yap("%d\n",x) periodt
+        }  
+    }  
+      
+    itsGiving based periodt
+}
+
