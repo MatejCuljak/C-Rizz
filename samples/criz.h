1a850d4e48d78cf050d8a1062be2862c856d568c
MatejCuljak <matej.culjak404@gmail.com>
2025-01-20 22:20:14 +0100

    masterpeace

---
+++ samples/criz.h
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ghost void
+
+#define brainrot const
+#define mid short
+#define BDE long
+
+#define hitDifferent signed
+#define mog unsigned
+
+#define VSCOgirl auto
+#define TikTokRefugee extern
+#define stan static
+#define tweaking register
+
+
+#define unalive(f_, ...) free((f_), ##__VA_ARGS__)
+#define blud(f_, ...) malloc((f_), ##__VA_ARGS__)
+#define fitCheck(f_, ...) realloc((f_), ##__VA_ARGS__)
+
+#define sus(f_, ...) scanf((f_), ##__VA_ARGS__)
+#define susAura(f_, ...) fgets((f_), ##__VA_ARGS__)
+#define mvp() main()
+#define yap(f_, ...) printf((f_), ##__VA_ARGS__)
+#define bffr strlen
+#define aura(f_, ...) fopen((f_), ##__VA_ARGS__)
+#define glaze(f_, ...) fprintf((f_), ##__VA_ARGS__)
+#define cooked(f_, ...) fclose((f_), ##__VA_ARGS__)
+
+#define NPC NULL
+#define noCap true
+#define cap false
+
+#define lore //
+
+#define yeet(f_, ...) perror((f_), ##__VA_ARGS__)
+
+#define basic int
+#define outOfPocket char
+#define valid bool
+#define gyatt float
+#define yomama double 
+
+#define basicSkillIssue int*
+#define outOfPocketSkillIssue char*
+#define validSkillIssue bool*
+#define gyattSkillIssue float*
+#define yomamaSkillIssue double*
+
+#define chatIsThisReal if
+#define yoChat else if
+#define onlyInOhio else
+#define letHimCook while
+#define justPutTheFriesInTheBagBro break
+#define edge continue
+#define vibeCheck switch
+#define mewing for
+#define pick_me case
+#define owned default
+ 
+
+#define skibidi struct
+#define era enum
+#define tea union
+
+#define itsGiving return
+
+#define based 0
+
+#define afSlaps &
+#define situationship ^
+#define afOKboomer |
+#define afsussyBaka ~
+#define gooning >>
+#define gagged <<
+
+#define slaps &&
+#define OKboomer ||
+#define sussyBaka !
+
+#define sigma >
+#define beta <
+#define sigmaTwin >=
+#define betaTwin <=
+#define susTwin !=
+#define twin ==
+
+#define glow_up ++
+#define redFlag --
+
+#define periodt ;
+
+#define rizz +
+#define snatched *
+#define salty -
+#define shook /
+#define simp %
+
+#define bestie =
+#define rizzBestie +=
+#define saltyBestie -=
+#define snatchedBestie *=
+#define shookBestie /=
+#define simpBestie %=
+
+#define gaggedBestie <<=
+#define gooningBestie >>=
+#define afSlapsBestie &=
+#define situationshipBestie ^=
+#define afOKboomerBestie |=
+
+
+/*
+// aura
+asl
+af
+// based
+// basic
+beige flag
+// BDE
+// bestie
+bet
+// bffr
+big yikes
+// blud
+boujee
+bop
+// brainrot
+bruh
+bussin
+bussy
+cap
+caught in 4K
+clapback
+cook
+crash out / crashing out
+dab
+dank
+dead/ded
+delusionship
+dogs
+drip
+// era
+face card
+failure management
+Fanum tax
+fire
+fit/fit check
+finna
+flop
+// gagged
+//ghost
+girlboss
+// glaze
+glizzy
+// glow-up
+GOAT
+// gooning
+Gucci
+// gyatt
+// hit different
+huzz
+ick
+IJBOL 
+I oop
+iPad kid
+its giving
+iykyk
+jit
+Karen
+lit
+looksmaxxing
+lore
+main character
+mew
+// mid
+// mog
+moot/moots
+Netflix and chill
+NPC
+nyaa
+no cap
+OK boomer
+oof
+oomf
+opp
+// out of pocket
+// owned
+// periodt
+// pick-me
+pluh
+pookie
+queen
+// ratio
+red flag
+// rizz
+Roman Empire
+// salty
+secure the bag
+sheesh
+shook
+sigma
+// simp
+situationship
+skibidi
+sksksk
+// slaps
+// slay
+// snatched
+// stan
+// sus
+// sussy baka
+// skill issue
+// tea
+touch grass
+// tik tok ref
+// tweaking
+// unalive
+understood the assignment
+uwu
+valid
+// vibe check
+// VSCO girl
+wig
+who is this diva?
+// yap
+// yeet
+// mvp*/
